8284693f7430e86a592e6aa08be5b66165e43b5f
albert-yw-lin <albert.yw.lin@gmail.com>
2025-08-06 10:41:29 -0700

    add a convenient script to setup env var

---
+++ run_before_everything.sh
@@ -0,0 +1,6 @@
+export PYTHONPATH=$(pwd)
+export CUDA_HOME=/viscam/u/ywlin/miniconda3/envs/drivestudio
+export LD_LIBRARY_PATH=$CUDA_HOME/lib:$LD_LIBRARY_PATH
+export PATH=$CUDA_HOME/bin:$PATH
+export LIBRARY_PATH=$CUDA_HOME/lib:$LIBRARY_PATH
+export CPATH=$CUDA_HOME/include:$CPATH
