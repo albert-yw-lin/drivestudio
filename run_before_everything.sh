74581e8f89b73a11105f9fae3d356b815be0a63a
albert-yw-lin <albert.yw.lin@gmail.com>
2025-08-11 11:23:11 -0700

    edit env var export

--- run_before_everything.sh
+++ run_before_everything.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=$(pwd)
-export CUDA_HOME=/viscam/u/ywlin/miniconda3/envs/drivestudio
-export LD_LIBRARY_PATH=$CUDA_HOME/lib:$LD_LIBRARY_PATH
+export CUDA_HOME=/root/miniforge3/envs/drivestudio #NOTE: You need to set this to your CUDA installation path
+export LD_LIBRARY_PATH=$CUDA_HOME/lib/python3.9/site-packages/torch/lib:$CUDA_HOME/lib:$LD_LIBRARY_PATH
 export PATH=$CUDA_HOME/bin:$PATH
 export LIBRARY_PATH=$CUDA_HOME/lib:$LIBRARY_PATH
 export CPATH=$CUDA_HOME/include:$CPATH
