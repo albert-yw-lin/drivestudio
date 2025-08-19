2fe61de8b8ed9e2d5a594dde60833c45736f1bbf
albert-yw-lin <albert.yw.lin@gmail.com>
2025-08-19 00:50:17 -0700

    trivial

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,2 +1,2 @@
-ckpt_path=outputs/nuscenes5/omnire_full/checkpoint_final.pth
+ckpt_path=outputs/nuscenes5/omnire_full_depth_0/checkpoint_final.pth
 python -m tools.eval --resume_from $ckpt_path
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,11 +1,11 @@
 config_file=configs/omnire_extended_cam.yaml
 output_root=outputs
 project=nuscenes5
-expname=omnire_full_test
+expname=omnire_full_ssim_0_4
 dataset=nuscenes/6cams
 scene_idx=5
 start_timestep=0 # start frame index for training
-end_timestep=10 # end frame index, -1 for the last frame
+end_timestep=-1 # end frame index, -1 for the last frame
 
 
 python -m tools.train \
